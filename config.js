14322dfa002dfda3b949b69c01f3592a200a9cb6
Radiant911 <164698830+Radiant911@users.noreply.github.com>
2026-02-08 01:20:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/du0ntuucx/video/upload/v1770495539/Soulja_Boy_-_Kiss_Me_Thru_The_Phone_Lyrics_ft._Sammie_1_dvkj50.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
